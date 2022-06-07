40f4dfea59125c0d67919d5ebb0e9c1dc66c746c
HuangKai <mail.huangkai@gmail.com>
2022-06-07 22:16:32 +0800

    Added a script about zhihu

---
+++ Close-the-login-modal-of-Zhihu.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         Close the login modal of Zhihu
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       Kay
+// @match        https://*.zhihu.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=zhihu.com
+// @grant        none
+// @updateURL    none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function removeElement(){
+        var ele = document.querySelector('.Modal-wrapper');
+        ele.parentNode.removeChild(ele);
+        console.log('Removed Zhihu Login Modal');
+    }
+
+    var timer = window.setTimeout(removeElement,100);
+    //window.clearTimeout(timer);// 清除定时器
+})();
+
+
